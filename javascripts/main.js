f61a19bcdc685f419ab9519f8d0a985b81e26e36
Sanyyouisf <sanymamdoh@yahoo.com>
2017-04-23 01:14:31 -0500

    FINISH

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,105 +1,103 @@
 $(document).ready(function() {
 
-    $("#xmenBtn").click((event) => {
-            event.preventDefault();
-            $("#logo").addClass("hidden");
-            // console.log($(event.currentTarget));
-            // dataGetter();
-            // loadCharacters();
-            // loadGenders();
-            // loadTeams();
+    //variables
+    const actorsArray = [];
+    let teamToPrint = "";
+
+    //event for the button
+    $(".btn").click((event) => {
+        $("#logo").addClass("hidden");
+        event.preventDefault();
+        dataGetter();
+        teamToPrint = ($(event.currentTarget).attr('id'));
+    });
 
 
-
-            const loadCharacters = () => {
-                return new Promise((resolve, reject) => {
-                    $.ajax("./db/characters.json")
-                        .done((data1) => {
-                            resolve(data1.characters);
-                            console.log("data1 characters :", data1);
-                        })
-                        .fail((error) => reject(error));
-                });
-            };
-
-            const loadGenders = () => {
-                return new Promise((resolve, reject) => {
-                    $.ajax("./db/genders.json")
-                        .done((data2) => {
-                            resolve(data2.genders);
-                            console.log("data2 characters :", data2);
-                        })
-                        .fail((error) => reject(error));
-                });
-            };
-
-            const loadTeams = () => {
-                return new Promise((resolve, reject) => {
-                    $.ajax("./db/teams.json")
-                        .done((data3) => {
-                            resolve(data3.teams);
-                            console.log("data3 characters :", data3);
-                        })
-                        .fail((error) => reject(error));
-                });
-            };
-
-            var actorsArray = [];
-
-            const WriteToDom = (actorsArray) => {
-                let domString = "";
-                for (let i = 0; i < actorsArray.length; i++) {
-                    domString += `<div class="col-md-3 col">`;
-                    domString += `<p class="name">${actorsArray[i].name}</p>`;
-                    //add gender boarder
-                    if (actorsArray[i].gender_id === 0) {
-                        domString += `<img  class= "image fBoarder" src="${actorsArray[i].image}">`;
-                    } else if (actorsArray[i].gender_id === 1) {
-                        domString += `<img  class= "image mBoarder" src="${actorsArray[i].image}">`;
-                    }
-                    //add description
-                    if (actorsArray[i].gender_id === 0 && actorsArray[i].description === "") {
-                        domString += `<p class="description"> abcde fghij klmno pqrst uvwxy z </p>`;
-                    } else if (actorsArray[i].gender_id === 1 && actorsArray[i].description === "") {
-                        domString += `<p class="description"> 1234567890 </p>`;
-                    }
-
-                    domString += `<p class="description">${actorsArray[i].description}</p>`;
-                    domString += `</div>`;
-                }
-                $("#output").append(domString)
-            };
+    const loadCharacters = () => {
+        return new Promise((resolve, reject) => {
+            $.ajax("./db/characters.json")
+                .done((data1) => resolve(data1.characters))
+                .fail((error) => reject(error));
+        });
+    };
 
 
+    const loadGenders = () => {
+        return new Promise((resolve, reject) => {
+            $.ajax("./db/genders.json")
+                .done((data2) => resolve(data2.genders))
+                .fail((error) => reject(error));
+        });
+    };
 
 
-            const checkForTeam = (characters, teams) => {
-                for (let i = 0; i < characters.length; i++) {
-                    for (let j = 0; j < teams.length; j++) {
-                        if (characters[i].team_id === teams[j].id) {
-                            var teamName = teams[j].name;
-                            characters[i].teamName = teamName;
-                            actorsArray.push(characters[i]);
-                        };
-                    };
+    const loadTeams = () => {
+        return new Promise((resolve, reject) => {
+            $.ajax("./db/teams.json")
+                .done((data3) => resolve(data3.teams))
+                .fail((error) => reject(error));
+        });
+    };
+
+
+    const WriteToDom = (actorsArray) => {
+        let domString = "";
+        let j=0 ;        
+        for (let i = 0; i < actorsArray.length; i++) {
+        	if ((j%4) ===0){
+				//creating a row
+		 		domString += `<div class="row">`;
+		 	}
+		 	j++;
+        	// domString += `<div class="row">`
+            domString += `<div class="col-md-3 col">`;
+            domString += `<p class="name">${actorsArray[i].name}</p>`;
+            //add gender boarder
+            if (actorsArray[i].gender_id === 0) {
+                domString += `<center><img  class= "image fBoarder" src="${actorsArray[i].image}"></center>`;
+            } else if (actorsArray[i].gender_id === 1) {
+                domString += `<center><img  class= "image mBoarder" src="${actorsArray[i].image}"></center>`;
+            }
+            //add description
+            if (actorsArray[i].gender_id === 0 && actorsArray[i].description === "") {
+                domString += `<p class="description"> abcde fghij klmno pqrst uvwxy z </p>`;
+            } else if (actorsArray[i].gender_id === 1 && actorsArray[i].description === "") {
+                domString += `<p class="description"> 1234567890 </p>`;
+            }
+
+            domString += `<p class="description">${actorsArray[i].description}</p>`;
+            domString += `</div>`;//for col
+            if ((j%4) ===0){
+			// closing the row after three columns
+		 		domString +=`</div>`;
+			}
+    	};
+    	$("#output").append(domString);
+    };
+
+
+    //to check which team to print depending on the button
+    const checkForTeam = (characters, teams, teamToPrint) => {
+        for (let i = 0; i < characters.length; i++) {
+            for (let j = 0; j < teams.length; j++) {
+                if (characters[i].team_id === teams[j].id && teams[j].name === teamToPrint) {
+                    let teamName = teams[j].name;
+                    characters[i].teamName = teamName;
+                    actorsArray.push(characters[i]);
                 };
-                console.log("actorsArray all is :", actorsArray);
             };
+        };
+    };
 
 
-
-            Promise.all([loadCharacters(), loadGenders(), loadTeams()])
-                .then((result) => {
-                    checkForTeam(result[0], result[2]);
-                    WriteToDom(actorsArray);
-                });
-
-        })
-        // .catch(function(error) {
-        //     console.log("the user is ", error.statusText);
-        //     alert("the user is ", error.statusText);
-        // });
-        // };
-
+    const dataGetter = () => {
+        Promise.all([loadCharacters(), loadGenders(), loadTeams()])
+            .then((result) => {
+                checkForTeam(result[0], result[2], teamToPrint);
+                WriteToDom(actorsArray);
+            }) 
+        .catch(function(error) {
+        });
+    };
 
 });
